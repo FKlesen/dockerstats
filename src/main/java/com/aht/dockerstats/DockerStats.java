0bc6c0cb0a386e591da23b0175742a3530650801
FKlesen <47566958+FKlesen@users.noreply.github.com>
2019-02-20 15:54:39 +0100

    Update DockerStats.java

--- src/main/java/com/aht/dockerstats/DockerStats.java
+++ src/main/java/com/aht/dockerstats/DockerStats.java
@@ -1,14 +1,17 @@
 package com.aht.dockerstats;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class DockerStats {
 
 	public static void main(String[] args) throws IOException {
+
 		String query = "docker stats --no-stream --format " +
 			"\"{" + 
 				"\\\"id\\\":\\\"{{ .ID }}\\\"," +
@@ -21,14 +24,35 @@ public class DockerStats {
 				"\\\"pids\\\":\\\"{{ .PIDs }}\\\"" +
 			"}\"";
 		
-		Process process = Runtime.getRuntime().exec(query);
+		Process process = Runtime.getRuntime().exec(query);		
+		
 		BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
 		
 		String line;
+		Containers containers = new Containers();
+		
 		while((line = input.readLine()) != null) {
 //			System.out.println(line); // debug
-			Container test = new ObjectMapper().readerFor(Container.class).readValue(line);
-			System.out.println(test.getName() + ": " + test.getCpu_perc()); // debug
+			Container con = new ObjectMapper().readerFor(Container.class).readValue(line);
+
+			Process process2 = Runtime.getRuntime().exec("docker container port " + con.getId());
+			BufferedReader input2 = new BufferedReader(new InputStreamReader(process2.getInputStream()));
+			String line2;
+			while((line2 = input2.readLine()) != null) {
+				if (line2 != "") {
+//					System.out.println(line2); // debug
+					con.addPortmapping(new PortMapping(line2.split("/| \\-\\> |:")));
+				}
+			}
+			
+			containers.addContainer(con);
+		}
+		
+		try (
+			FileOutputStream fos = new FileOutputStream ("containers.ser");
+			ObjectOutputStream oos = new ObjectOutputStream (fos)
+		) {
+			oos.writeObject (containers);
 		}
 	}
 
